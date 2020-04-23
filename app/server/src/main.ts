e94f5e5585352a14598a7d67bd5d206e39c0116b
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-04-23 17:09:26 -0300

    criado module user

--- app/server/src/main.ts
+++ app/server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+ 
+  const PORT=process.env.PORT || 3000
+  await app.listen((PORT),()=>{
+    console.log(PORT)
+  });
 }
 bootstrap();
